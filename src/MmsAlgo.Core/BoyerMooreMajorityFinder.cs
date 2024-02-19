ce606ba728d8dd36ba98e8add360f4b4068fde2b
Kwezilomso Mhaga <kwezimhaga@live.com>
2024-02-19 16:46:02 +0200

    added code for boyer moore's algorithm

--- src/MmsAlgo.Core/BoyerMooreMajorityFinder.cs
+++ src/MmsAlgo.Core/BoyerMooreMajorityFinder.cs
@@ -1,6 +1,34 @@
 namespace MmsAlgo.Core;
 
-public class BoyerMooreMajorityFinder
+public class BoyerMooreMajorityFinder : IMajorityElementFinder
 {
-    
+    public string FindElement(int[] sample)
+    {
+        var threshold = sample.Length / 2M;
+        var counter = 1;
+        var majority = sample[0];
+        for (int i = 1; i < sample.Length; i++)
+        {
+            if (counter == 0)
+            {
+                majority = sample[i];
+                counter = 1;
+            }
+            
+            if (sample[i] == majority)
+            {
+                counter += 1;
+            }
+            else
+            {
+                counter -= 1;
+            }
+        }
+
+        if (majority <= threshold)
+        {
+            return "No majority element";
+        }
+        return majority.ToString();
+    }
 }
