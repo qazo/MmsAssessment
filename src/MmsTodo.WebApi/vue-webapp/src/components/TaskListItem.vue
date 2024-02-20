135c7617d383c9c27ef6bb3e2e3dc05d10e00308
Kwezilomso Mhaga <kwezimhaga@live.com>
2024-02-20 09:36:07 +0200

    chore: change color of complete button

--- src/MmsTodo.WebApi/vue-webapp/src/components/TaskListItem.vue
+++ src/MmsTodo.WebApi/vue-webapp/src/components/TaskListItem.vue
@@ -76,7 +76,8 @@ function handleTaskComplete() {
 			<div class="col-auto">
 				<div class="btn-group h-100">
 					<button :disabled="model.isCompleted" type="button" @click="handleTaskComplete"
-						class="btn btn-light rounded-0 border-start" title="Complete">
+						class="btn btn-light rounded-0 border-start text-opacity-75"
+						:class="{ 'text-success': !model.isCompleted }" title="Complete">
 						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
 							class="bi bi-check-circle" viewBox="0 0 16 16">
 							<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
